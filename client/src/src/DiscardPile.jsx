5b7e347c7f9c5fba083d5201b231c6ae94c36c2c
Jason-Goon <156106640+Jason-Goon@users.noreply.github.com>
2024-02-13 02:18:23 +0200

    Update DiscardPile.jsx

--- client/src/src/DiscardPile.jsx
+++ client/src/src/DiscardPile.jsx
@@ -3,13 +3,17 @@ import React from 'react';
 import Card from './Card';
 import './DiscardPile.css'; // Ensure this is correctly pointing to your CSS file
 
-const DiscardPile = ({ cards }) => {
+const DiscardPile = ({ cards, onSelectCard, selectedIndices }) => {
     return (
         <div className="discardPile">
             <h3>Discard Pile</h3>
             <div className="cards"> {/* This container will hold the cards in a row */}
                 {cards.map((card, index) => (
-                    <Card key={index} value={card.value} suit={card.suit} />
+                    // Wrap the Card component in a div that can be clicked
+                    <div key={index} onClick={() => onSelectCard(index)} 
+                         className={`card-container ${selectedIndices.includes(index) ? 'selected' : ''}`}>
+                        <Card value={card.value} suit={card.suit} />
+                    </div>
                 ))}
             </div>
         </div>
@@ -17,3 +21,4 @@ const DiscardPile = ({ cards }) => {
 };
 
 export default DiscardPile;
+
